9d0dc1bea8ab464d97442b3db9a0e40b71db2c1b
Florian Frank <flori@ping.de>
2013-09-15 11:35:16 +0200

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ablass::Application.config.secret_key_base = '9eaeac39d9bd270770296166e58d9752cc864033bbc68510b75422f66923d9d155e47c179edece6b9ad6adab52b3e495c64967e84a088c5466118776c3577bdf'
