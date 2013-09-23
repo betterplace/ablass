34d7d138798a463a67efb734a67527679f658b16
Florian Frank <flori@ping.de>
2013-09-23 17:06:27 +0200

    Use local betterplace for development
    Also use bp42 for production for starters

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Ablass::Application.configure do
 
   # betterplace API root
   config.betterplace_api = -> path, locale: I18n.locale {
-    "http://api.betterplace.org/%s/api_v4/%s" % [ locale, path.sub(/\A\/*/, '') ]
+    "http://api.bp42.com/%s/api_v4/%s" % [ locale, path.sub(/\A\/*/, '') ]
   }
 
   #config.asset_host = 'http://www.ablass-app.de'
